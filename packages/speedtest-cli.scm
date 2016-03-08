6dfc76e961e671b56aa920adbdbb41eb565a1844
Efraim Flashner <efraim@flashner.co.il>
2016-03-08 08:17:50 +0200

    Reorganize repo to cut down clutter

--- packages/speedtest-cli.scm
+++ packages/speedtest-cli.scm
@@ -15,7 +15,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (efraim packages speedtest-cli)
+(define-module (packages speedtest-cli)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix download)
   #:use-module (guix packages)
