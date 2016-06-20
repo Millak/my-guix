52d86d1b3053ee426326c406f88f1e7d45d84a83
Efraim Flashner <efraim@flashner.co.il>
2016-06-20 14:30:07 +0300

    equate: new variable

---
+++ packages/equate.scm
@@ -0,0 +1,60 @@
+;;; Copyright © 2016 Efraim Flashner <efraim@flashner.co.il>
+;;;
+;;; This file is an addendum to GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (packages equate)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages enlightenment)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages pkg-config))
+
+(define-public equate
+  (package
+    (name "equate")
+    (version "20160611")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://git.enlightenment.org/apps/equate.git/")
+               (commit "5ee65c6bc64f71198a92244aa6abbb63c122e35d")))
+        (file-name (string-append name "-" version "-checkout"))
+        (sha256
+         (base32
+          "18lr9r7v19xhg17j6xc8gcm1rhiljj7mjb5dc7w31j2z1v6nsqd0"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'autoconf
+           (lambda _ (zero? (system* "autoreconf" "-vfi")))))))
+    (native-inputs
+     `(("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("gettext" ,gnu-gettext)
+       ("pkg-config" ,pkg-config)))
+    (inputs
+     `(("efl" ,efl)
+       ("elementary" ,elementary)))
+    (home-page "https://www.enlightenment.org")
+    (synopsis "Elementary based calculator")
+    (description "Elementary based calculator")
+    (license license:bsd-2)))
