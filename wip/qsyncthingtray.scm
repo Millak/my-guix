7ec3202080bea5647dbf636876c1b65254e53b58
Efraim Flashner <efraim@flashner.co.il>
2016-07-12 21:31:39 +0300

    wip: qsyncthingtray: got started

---
+++ wip/qsyncthingtray.scm
@@ -0,0 +1,66 @@
+;;; Copyright © 2016 Efraim Flashner <efraim@flashner.co.il>
+;;;
+;;; This file is an addendum to GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (wip qsyncthingtray)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (guix packages)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages qt))
+
+(define-public qsyncthingtray
+  (package
+    (name "qsyncthingtray")
+    (version "0.5.1")
+    (source
+      (origin
+        (method url-fetch)
+        (uri (string-append
+               "https://github.com/sieren/QSyncthingTray/archive/"
+               version ".tar.gz"))
+        (file-name (string-append name "-" version ".tar.gz"))
+        (sha256
+         (base32
+          "1hq8vgqjhzngfsh6wmq6wrigx41cwm3j8vncmp4wgafdp64gmi34"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+         (replace 'configure
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let ((out (assoc-ref outputs "out")))
+               (zero? (system* "qmake"
+                               (string-append "PREFIX=" out)))))))))
+    (inputs
+     `(("qt" ,qt)))
+    (home-page "https://github.com/sieren/QSyncthingTray")
+    (synopsis "Traybar Application for Syncthing")
+    (description
+     "A traybar application for syncthing.
+@enumerate
+@item Shows number of connections at a glance.
+@item Traffic statistics about incoming, outgoing and total throughput.
+@item Launches Syncthing and Syncthing-iNotifier if specified.
+@item Quickly pause Syncthing with one click.
+@item Last Synced Files - Quickly see the recently synchronised files and open their folder.
+@item Quick Access to all shared folders.
+@item Presents Syncthing UI in a separate view instead of using the browser.
+@item Supports authenticated HTTPS connections.
+@item Uses System Notifications about current connection status.
+@item Toggle for monochrome icon.
+@end enumerate\n")
+    (license license:lgpl2.1)))
