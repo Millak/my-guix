f6936e0b921d23a7e207b748633b4956281c8635
Efraim Flashner <efraim@flashner.co.il>
2016-10-23 17:17:24 +0300

    wip: tar: typo

--- wip/tar.scm
+++ wip/tar.scm
@@ -15,7 +15,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (packages tar)
+(define-module (wip tar)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (gnu packages base)
