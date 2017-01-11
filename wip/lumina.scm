65cfe187f5bdcde4547fb2c00acc1934cd74f447
Efraim Flashner <efraim@flashner.co.il>
2017-01-11 11:56:34 +0200

    wip: lumina: Almost ready?

--- wip/lumina.scm
+++ wip/lumina.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2016 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2016, 2017 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is an addendum to GNU Guix.
 ;;;
@@ -20,55 +20,91 @@
   #:use-module (guix build utils)
   #:use-module (guix download)
   #:use-module (guix packages)
+  #:use-module (guix utils) ; substitute-keyword-arguments
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system trivial)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages kde-frameworks)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages qt)
+  #:use-module (gnu packages wm) ; fluxbox
+  #:use-module (gnu packages xdisorg) ; xscreensaver
   #:use-module (gnu packages xorg))
 
 (define-public lumina
   (package
     (name "lumina")
-    (version "1.1.0-p1")
+    (version "1.2.0-p1")
     (source
       (origin
         (method url-fetch)
         (uri (string-append
-               "https://github.com/trueos/lumina/archive/v" version ".tar.gz"))
+              "https://github.com/trueos/lumina/archive/v" version ".tar.gz"))
         (file-name (string-append name "-" version ".tar.gz"))
         (sha256
          (base32
-          "0vdmqa0wviv9iy33nagmq5zn33fcq904nv72q4czqb3dd88qq5h3"))))
+          "0jdxcpygivc6q3srfb9y91rfp4ksx56mpvzbbyf06a9zzq964dpq"))
+        (modules '((guix build utils)))
+        (snippet
+         '(begin
+            ;; This creates the GuixSD specific Lumina config.
+            (copy-file "src-qt5/core/libLumina/LuminaOS-Gentoo.cpp"
+                       "src-qt5/core/libLumina/LuminaOS-GuixSD.cpp")
+            (substitute* "src-qt5/core/libLumina/LuminaOS-GuixSD.cpp"
+              (("Gentoo Linux") "GuixSD")
+              (("AppStoreShortcut\\(\\)")
+               (string-append "AppStoreShortcut(){ return \"\"; }//"))
+              (("\"/usr/") "PREFIX+\"usr")
+              (("\"/\"") "PREFIX")
+              )
+            ))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'fix-install-locations
-           (lambda _
-             (substitute* "src-qt5/core/libLumina/LuminaOS-Linux.cpp"
-               (("/usr/") (assoc-ref %outputs "out")))
-             (substitute* "src-qt5/OS-detect.pri"
-               (("/usr") (assoc-ref %outputs "out"))
-               (("L_SESSDIR=/usr/share/xsessions") ""))
-             #t))
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let ((fluxbox (assoc-ref inputs "fluxbox"))
+                   (out     (assoc-ref outputs "out")))
+               (substitute* "src-qt5/core-utils/lumina-config/pages/page_fluxbox_settings.cpp"
+                 (("LOS::AppPrefix\\(\\)+\\\"") (string-append "\"" fluxbox "/"))
+                 )
+               ;(substitute* "src-qt5/core/libLumina/LuminaOS-GuixSD.cpp"
+               ;  (("/usr/") (string-append out "/usr"))
+               ;  (("\"/\"") (string-append "\"" out "\""))
+               ;  )
+               (substitute* "src-qt5/OS-detect.pri"
+                 (("L_SESSDIR=/usr/share/xsessions") "")
+                 )
+               )
+               #t))
          (replace 'configure
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let ((out (assoc-ref outputs "out")))
-               (zero? (system* "qmake"
-                               (string-append "PREFIX=" out)))))))
-       ;#:make-flags (list (string-append "-L" (assoc-ref %outputs "out") "/lib"))
-       ))
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((qttools (assoc-ref inputs "qttools"))
+                    (lrelease (string-append qttools "/bin/lrelease"))
+                    (out (assoc-ref outputs "out")))
+               (zero? (system* "qmake" "LINUX_DISTRO=GuixSD"
+                               (string-append "LRELEASE=" lrelease)
+                               (string-append "PREFIX=" out)))))))))
+    (propagated-inputs
+     `(("fluxbox" ,fluxbox))) ; Also needed at runtime
+    (native-inputs
+     `(("qttools" ,qttools)))
     (inputs
-     `(
-       ("libxcb" ,libxcb)
+     `(("libxcb" ,(package
+                    (inherit libxcb)
+                    (arguments
+                     (substitute-keyword-arguments (package-arguments libxcb)
+                       ((#:configure-flags flags)
+                        `(cons* "--enable-xinput" ,flags))))))
        ("qtbase" ,qtbase)
-       ("qtdeclarative" ,qtdeclarative)
        ("qtmultimedia" ,qtmultimedia)
        ("qtsvg" ,qtsvg)
-       ("qttools" ,qttools)
        ("qtx11extras" ,qtx11extras)
        ("xcb-util" ,xcb-util)
        ("xcb-util-image" ,xcb-util-image)
-       ("xcb-util-wm" ,xcb-util-wm)
-       ))
+       ("xcb-util-wm" ,xcb-util-wm)))
     (home-page "https://lumina-desktop.org/")
     (synopsis "Lumina Desktop Environment")
     (description
@@ -78,4 +114,34 @@ approach, allowing the entire interface to be assembled/arranged by each
 individual user as desired, with a system-wide default layout which can be
 setup by the system administrator.  This allows every system (or user session)
 to be designed to maximize the individual user's productivity.")
-    (license license:bsd-3)))
+    (license (list license:bsd-3
+                   license:cc-by-sa4.0)))) ; icon-theme
+
+(define-public luminaOS
+  (package
+    (name "luminaOS")
+    (version (package-version lumina))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments '(#:builder (mkdir %output)))
+    (propagated-inputs
+     `(
+       ("acpi" ,acpi) ; linux
+       ("dbus" ,dbus) ; glib
+       ("lumina" ,lumina)
+       ;("oxygen-icons" ,oxygen-icons) ; kde-frameworks
+       ;("pavucontrol" ,pavucontrol) ; pulseaudio
+       ("sysstat" ,sysstat) ; linux?
+       ("xscreensaver" ,xscreensaver) ; xdisorg
+       ))
+    (home-page "https://lumina-desktop.org/")
+    (synopsis "The Lumina Desktop Environment")
+    (description
+     "The Lumina Desktop Environment is a lightweight system interface that is
+designed for use on any Unix-like operating system.  It takes a plugin-based
+approach, allowing the entire interface to be assembled/arranged by each
+individual user as desired, with a system-wide default layout which can be
+setup by the system administrator.  This allows every system (or user session)
+to be designed to maximize the individual user's productivity.")
+    (license (list license:bsd-3
+                   license:cc-by-sa4.0)))) ; icon-theme
