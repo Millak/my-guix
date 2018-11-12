2ff34090d4f0d4969b76bff75b9f284dfef3125e
Efraim Flashner <efraim@flashner.co.il>
2018-11-12 19:44:44 +0200

    dfsg-non_free: fixup ravkav

--- dfsg/non-free/ravkav.scm
+++ dfsg/non-free/ravkav.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2017 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2017. 2018 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is an addendum to GNU Guix.
 ;;;
@@ -15,17 +15,19 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (wip ravkav)
+(define-module (dfsg non-free ravkav)
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (guix utils)
   #:use-module (guix build-system gnu)
+  #:use-module (gnu packages bootstrap) ; glibc-dynamic-linker
+  #:use-module (gnu packages elf)
   #:use-module (gnu packages security-token))
 
 (define-public ravkavonline
   (package
     (name "ravkavonline")
-    (version "1.1.0")
+    (version "1.4.0")
     (source
       (origin
         (method url-fetch)
@@ -33,34 +35,50 @@
                             "ravkavonline_" version "_amd64.deb"))
         (sha256
          (base32
-          "0kx1cjywbxl7gn0r81bdgr5m1ka3y45pm0fq1jgkfag1qnqm69hx"))))
+          "0kzfxv3vz03m9sym1b0x9k22aav1rbvfb46xz2zy9hzkcvirral8"))))
     (build-system gnu-build-system)
     (arguments
-     '(#:phases
+     `(#:phases
        (modify-phases %standard-phases
          (delete 'configure)
-         (delete 'build)
          (replace 'unpack
            (lambda* (#:key inputs #:allow-other-keys)
              (let ((source (assoc-ref inputs "source")))
-             (system* "ar" "x" source "data.tar.xz"))))
+               (invoke "ar" "x" source "data.tar.xz"))))
          (add-after 'unpack 'unpack-tarball
            (lambda _
-             (zero? (system* "tar" "xvf" "data.tar.xz"))))
+             (invoke "tar" "xvf" "data.tar.xz")))
+         (replace 'build
+           (lambda* (#:key inputs outputs #:allow-other-keys)
+             (let* ((ravkav  "usr/bin/ravkavonline")
+                    (desk    "usr/share/applications/ravkavonline.desktop")
+                    (out     (assoc-ref outputs "out"))
+                    (libc    (assoc-ref inputs "libc"))
+                    (ld-so   (string-append libc ,(glibc-dynamic-linker)))
+                    (libpcsc (assoc-ref inputs "pcsc-lite"))
+                    (rpath   (string-append libpcsc "/lib")))
+                 (invoke "patchelf" "--set-rpath" rpath ravkav)
+                 (invoke "patchelf" "--set-interpreter" ld-so ravkav)
+                 (substitute* desk
+                   (("usr") out)))
+             #t))
          (replace 'install
            (lambda* (#:key outputs #:allow-other-keys)
              (let ((out (assoc-ref outputs "out")))
                (with-directory-excursion "usr"
-                 (install-file "bin/ravkavonline" (string-append out "/bin/ravkavonline"))
+                 (install-file "bin/ravkavonline" (string-append out "/bin"))
                  (install-file "share/applications/ravkavonline.desktop"
-                               (string-append out "/share/applications/ravkavonline.desktop"))
+                               (string-append out "/share/applications"))
                  (install-file "share/doc/ravkavonline/LICENSE.txt"
-                               (string-append out "/share/doc/ravkavonline/LICENSE.txt"))))))
-         )
+                               (string-append out "/share/doc/ravkavonline")))
+               #t))))
        #:tests? #f))
+    (native-inputs
+     `(("patchelf" ,patchelf)))
     (inputs
      `(("pcsc-lite" ,pcsc-lite)))
     (home-page "https://ravkavonline.co.il/he/")
-    (synopsis "")
-    (description "")
+    (synopsis "Charge your ravkav at home")
+    (description "Save time and charge your ravkav at home.")
+    (supported-systems '("x86_64-linux"))
     (license #f)))
