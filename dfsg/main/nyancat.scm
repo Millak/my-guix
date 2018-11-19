126288eca5e5ea7aefa0d3ec395717dce39175f4
Efraim Flashner <efraim@flashner.co.il>
2018-11-19 09:47:43 +0200

    dfsg-main: Add nyancat.

---
+++ dfsg/main/nyancat.scm
@@ -0,0 +1,59 @@
+;;; Copyright © 2018 Efraim Flashner <efraim@flashner.co.il>
+;;;
+;;; This file is an addendum to GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (dfsg main nyancat)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix build-system gnu))
+
+(define-public nyancat
+  (package
+    (name "nyancat")
+    (version "1.5.2")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/klange/nyancat")
+               (commit version)))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32
+          "1mg8nm5xzcq1xr8cvx24ym2vmafkw53rijllwcdm9miiz0p5ky9k"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (delete 'configure) ; no configure script
+         (add-after 'unpack 'set-environmental-variables
+           (lambda _ (setenv "CC" (which "gcc")) #t))
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (bin (string-append out "/bin"))
+                    (man (string-append out "/share/man/man1")))
+               (install-file "src/nyancat" bin)
+               (install-file "nyancat.1" man))
+             #t)))))
+    (home-page "http://nyancat.dakko.us/")
+    (synopsis "Nyan cat telnet server")
+    (description
+     "This is an animated, color, ANSI-text telnet server that renders a loop
+of the Nyan Cat / Poptart Cat animation.")
+    (license license:ncsa)))
