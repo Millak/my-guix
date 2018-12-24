7f59a5c06e743201b539ee1e04f79c276e729c40
Efraim Flashner <efraim@flashner.co.il>
2018-12-24 17:24:08 +0200

    bidiv: build works

--- dfsg/main/bidiv.scm
+++ dfsg/main/bidiv.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2016 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2016, 2018 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is an addendum to GNU Guix.
 ;;;
@@ -15,13 +15,13 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (wip bidiv)
+(define-module (dfsg main bidiv)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix download)
   #:use-module (guix packages)
   #:use-module (guix build-system gnu)
-  #:use-module (gnu packages fribidi)
-  #:use-module (gnu packages pkg-config))
+  #:use-module (gnu packages)
+  #:use-module (gnu packages fribidi))
 
 (define-public bidiv
   (package
@@ -34,24 +34,39 @@
                             version ".orig.tar.gz"))
         (sha256
          (base32
-          "05p5m2ihxbmc1qsgs8rjlww08fy9859fhl7xf196p8g5qygqd7cv"))))
+          "05p5m2ihxbmc1qsgs8rjlww08fy9859fhl7xf196p8g5qygqd7cv"))
+        (patches (search-patches "bidiv-update-fribidi.patch"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
        (modify-phases %standard-phases
          (delete 'configure) ; no configure
+         (add-after 'unpack 'misc-fixes
+           (lambda _
+             (substitute* "bidiv.c"
+               (("FriBidiCharType") "FriBidiParType")
+               (("&c") "(char *)&c"))
+             #t))
          ;; We don't want to use the handwritten makefile
          (replace 'build
-           (lambda* (#:key (make-flags '()) #:allow-other-keys)
-             (zero?
-               (apply system* "gcc" "-o" "bidiv" "bidiv.c" make-flags)))))
-       #:make-flags (list (string-append "PREFIX=" %output))))
-    (native-inputs
-     `(("pkg-config" ,pkg-config)
-    ;   ))
-    ;(inputs
-    ; `(
-       ("fribidi" ,fribidi)))
+           (lambda* (#:key inputs #:allow-other-keys)
+             (let ((fribidi (assoc-ref inputs "fribidi")))
+               (invoke "gcc" "-o" "bidiv" "bidiv.c"
+                       ;; pkg-config --cflags fribidi
+                       (string-append "-I" fribidi "/include/fribidi")
+                       ;; pkg-config --libs fribidi
+                       (string-append "-L" fribidi "/lib") "-lfribidi"))))
+         (replace 'install
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (bin (string-append out "/bin"))
+                    (man (string-append out "/share/man/man1")))
+               (install-file "bidiv" bin)
+               (install-file "bidiv.1" man))
+             #t)))
+       #:tests? #f)) ; no tests
+    (inputs
+     `(("fribidi" ,fribidi)))
     (home-page "https://tracker.debian.org/pkg/bidiv")
     (synopsis "BiDi viewer - command-line tool displaying logical Hebrew/Arabic")
     (description "bidiv is a simple utility for converting logical-Hebrew input
