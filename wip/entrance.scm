e9491334fd4dc507cdfa87de4fe6b9480180b8f9
Efraim Flashner <efraim@flashner.co.il>
2019-07-04 15:15:51 +0300

    wip: add entrance

---
+++ wip/entrance.scm
@@ -0,0 +1,73 @@
+;;; Copyright © 2019 Efraim Flashner <efraim@flashner.co.il>
+;;;
+;;; This file is an addendum to GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (wip entrance)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix git-download)
+  #:use-module (guix packages)
+  #:use-module (guix utils)
+  #:use-module (guix build-system meson)
+  #:use-module (gnu packages enlightenment)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pkg-config))
+
+(define-public entrance
+  (package
+    (name "entrance")
+    (version "3.0.0_alpha6")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/Obsidian-StudiosInc/entrance")
+               (commit (string-append "v" version))))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32
+          "1kr4d2cj6llkhqxj0xx9qdd7ghma06z2ssi6mn0rg00jha77krj7"))))
+    (build-system meson-build-system)
+    (arguments
+     '(#:tests? #f  ; no tests
+       #:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'fix-source
+           (lambda* (#:key inputs #:allow-other-keys)
+             (let ((pam (assoc-ref inputs "pam")))
+               (substitute* "meson.build"
+                 (("/usr/include/security")
+                  (string-append pam "/include/security")))
+               #t)))
+         (add-after 'unpack 'set-home-dir
+           (lambda _
+             (setenv "HOME" (getenv "TMPDIR"))
+             #t)))))
+    (native-inputs
+     `(("gettext" ,gettext-minimal)
+       ("pkg-config" ,pkg-config)))
+    (inputs
+     `(("efl" ,efl)
+       ("pam" ,linux-pam)))
+    (home-page "https://github.com/Obsidian-StudiosInc/entrance")
+    (synopsis "Display Manager built on EFL")
+    (description "Entrance is a Unix Display/Login Manager written in
+Enlightenment Foundation Libraries (EFL).  Entrance allows a user to choose an X
+WM/Desktop session to launch upon successful login.  Entrance is alive and
+working again for logging into X sessions and eventually Wayland sessions!
+
+The project has been resurrected from the dead to live on once again...")
+    (license license:gpl3)))
