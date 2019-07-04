be91050aa8138ee467efa02365b741951752bc9b
Efraim Flashner <efraim@flashner.co.il>
2019-07-04 15:16:31 +0300

    wip: update dianara

--- wip/dianara.scm
+++ wip/dianara.scm
@@ -29,7 +29,7 @@
 (define-public dianara
   (package
     (name "dianara")
-    (version "1.3.4")
+    (version "1.4.2")
     (source
       (origin
         (method url-fetch)
@@ -37,7 +37,7 @@
                "mirror://savannah/dianara/dianara-v" version ".tar.gz"))
         (sha256
          (base32
-          "083bzbrrq2n3jk48643bcbh8dh21fgx62dzqj6ffsw13gygxjn62"))))
+          "1flkrcm26rwsl3mdkb484sihipv9swnlwvwgd50skiarczc466z6"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
@@ -45,8 +45,8 @@
          (replace 'configure
            (lambda* (#:key outputs #:allow-other-keys)
              (let ((out (assoc-ref outputs "out")))
-               (zero? (system* "qmake"
-                               (string-append "PREFIX=" out)))))))))
+               (invoke "qmake"
+                       (string-append "PREFIX=" out))))))))
     (inputs
      `(("file" ,file) ; libmagic
        ("qca" ,qca)
@@ -61,70 +61,52 @@ need to use a web browser.")
     (license license:gpl2+)))
 
 (define-public qoauth
-  ;; The last release is from 2010, and support for qt5 has been added since
-  (let ((commit "02fbc13a42d945b703a28a49d71c02b20c76a0b8")
-        (revision "1"))
-    (package
-      (name "qoauth")
-      (version (string-append "1.0.1-" revision "." (string-take commit 7)))
-      (source
-        (origin
-          (method git-fetch)
-          (uri (git-reference
-                (url "https://github.com/ayoy/qoauth.git")
-                (commit commit)))
-          (file-name (git-file-name name version))
-          (sha256
-           (base32
-            "0lnzgpdfjcx7szw1ibm4sqrsd7ahnnrh7i7bxsj63hyicnaihxh8"))))
-      (build-system gnu-build-system)
-      (arguments
-       `(#:phases
-         (modify-phases %standard-phases
-           (replace 'configure
-             (lambda* (#:key outputs #:allow-other-keys)
-               (let ((out (assoc-ref outputs "out")))
-                 (zero? (system* "qmake"
-                                 (string-append "PREFIX=" out)))))))
-         #:tests? #f ; F*** tests
-         ))
-      (inputs
-       `(("qca" ,qca)
-         ("qtbase" ,qtbase)))
-      (home-page "https://github.com/ayoy/qoauth")
-      (synopsis "Qt-based client implementation of the OAuth authorization scheme")
-      (description
-       "QOAuth is a Qt-based C++ implementation of an interface to services
-  using OAuth authorization scheme.")
-      (license license:lgpl2.1+))))
-
-(define-public qoauth-qt4
-  (package (inherit qoauth)
-    (name "qoauth-qt4")
+  (package
+    (name "qoauth")
+    (version "2.0.0")
+    (source
+      (origin
+        (method git-fetch)
+        (uri (git-reference
+               (url "https://github.com/ayoy/qoauth.git")
+               (commit (string-append "v" version))))
+        (file-name (git-file-name name version))
+        (sha256
+         (base32
+          "1b2jdqs526ac635yb2whm049spcsk7almnnr6r5b4yqhq922anw3"))))
+    (build-system gnu-build-system)
     (arguments
      `(#:phases
        (modify-phases %standard-phases
-         (add-after 'unpack 'patch-qtcrypto
-           (lambda _
-             ;; Qca puts QtCrypto in include/Qca-qt5/QtCrypto
-             (substitute* '("tests/ut_interface/ut_interface.h"
-                            "src/interface.h" "src/interface.cpp")
-                          (("QtCrypto") "QtCrypto/QtCrypto"))
-             #t))
+         (add-after 'unpack 'fix-source
+           (lambda* (#:key inputs #:allow-other-keys)
+             (let ((qca (assoc-ref inputs "qca")))
+               (substitute* "src/src.pro"
+                 (("lib64") "lib"))
+               (substitute* '("tests/ut_interface/ut_interface.h"
+                              "src/interface.h"
+                              "src/interface.cpp")
+                 (("QtCrypto") "Qca-qt5/QtCrypto/qca.h"))
+               ;(substitute* '("tests/ut_interface/ut_interface.h"
+               ;               "tests/ut_interface/ut_interface.cpp"
+               ;               "src/interface_p.h"
+               ;               "src/interface.h"
+               ;               "src/interface.cpp")
+               ;  (("QCA::") 
+               #t)))
          (replace 'configure
            (lambda* (#:key outputs #:allow-other-keys)
              (let ((out (assoc-ref outputs "out")))
-               (zero? (system* "qmake"
-                               (string-append "PREFIX=" out)))))))
+               (invoke "qmake"
+                       (string-append "PREFIX=" out))))))
+       #:tests? #f ; F*** tests
        ))
     (inputs
-     `(("qca-qt4" ,qca-qt4)
-       ("qt-4" ,qt-4)))))
-
-
-(define-public qca-qt4
-  (package (inherit qca)
-    (name "qca-qt4")
-    (inputs
-     `(("qt-4" ,qt-4)
-       ,@(alist-delete "qtbase" (package-inputs qca))))))
+     `(("qca" ,qca)
+       ("qtbase" ,qtbase)))
+    (home-page "https://github.com/ayoy/qoauth")
+    (synopsis "Qt-based for OAuth authorization")
+    (description
+     "QOAuth is a Qt-based C++ implementation of an interface to services
+using OAuth authorization scheme.")
+    (license license:lgpl2.1+)))
