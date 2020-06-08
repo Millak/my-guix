45eacc2198f1be28dfba9b347c0f38469831e8d2
Efraim Flashner <efraim@flashner.co.il>
2020-06-08 14:22:31 +0300

    dfsg: move keybase to contrib

--- dfsg/contrib/keybase.scm
+++ dfsg/contrib/keybase.scm
@@ -15,7 +15,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (dfsg main keybase)
+(define-module (dfsg contrib keybase)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix build utils)
   #:use-module (guix git-download)
