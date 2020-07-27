57ebed6f9eae91d53dfe92f0d423347c1aeb5e9b
Efraim Flashner <efraim@flashner.co.il>
2020-07-27 10:43:13 +0300

    dfsg-main: Add pipx

--- dfsg/main/pipx.scm
+++ dfsg/main/pipx.scm
@@ -15,36 +15,28 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (wip pipx)
+(define-module (dfsg main pipx)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix download)
   #:use-module (guix packages)
-  ;#:use-module (guix utils)
   #:use-module (guix build-system python)
   #:use-module (gnu packages check)
-  #:use-module (gnu packages python-xyz)
-  )
+  #:use-module (gnu packages python-xyz))
 
 (define-public pipx
   (package
     (name "pipx")
-    (version "0.15.1.3")
+    (version "0.15.4.0")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "pipx" version))
         (sha256
          (base32
-          "0bmpri17phgziqhasp332ygiaa6lbw6pf6ybii1fhk7a96akmah1"))))
+          "1dawivwkg28nc6dajbz84yqqbqn1sy0hn4a89sx4bpzdqshqa0mr"))))
     (build-system python-build-system)
-    (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (replace 'check
-           (lambda _
-             (setenv "PYTHONPATH" (string-append (getcwd) "/tests:" (getenv "PYTHONPATH")))
-             (invoke "python3" "-m" "unittest" "tests.test_pipx.PipxStaticTests"))))))
-    (propagated-inputs
+    (arguments '(#:tests? #f))  ; tests not included
+    (inputs
      `(("python-argcomplete" ,python-argcomplete)
        ("python-userpath" ,python-userpath)))
     (home-page "https://github.com/pipxproject/pipx")
@@ -65,35 +57,20 @@ their CLI applications so you can run them from anywhere.
 (define-public python-userpath
   (package
     (name "python-userpath")
-    (version "1.3.0")
+    (version "1.4.1")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "userpath" version))
         (sha256
          (base32
-          "0s2ljqd40ifm8fj9jba2crjmj71jpr8rww3bbr662b3ka2k8n2yx"))))
+          "0mfjmvx286z1dmnrc7bm65x8gj8qrmkcyagl0vf5ywfq0bm48591"))))
     (build-system python-build-system)
-    (arguments
-     '(
-       #:phases
-       (modify-phases %standard-phases
-         (replace 'check
-           (lambda* (#:key outputs #:allow-other-keys)
-             (setenv "TOXENV" "py37")
-             (setenv "TOX_ENV_NAME" "testenv")
-             (setenv "PATH" (string-append (assoc-ref outputs "out") "/bin:" (getcwd) ":" (getenv "PATH")))
-             (setenv "PYTHONPATH" (string-append (getcwd) ":" (getenv "PYTHONPATH")))
-             ;(invoke "python" "-m" "pytest" "-v")
-             #t)))
-       ))
+    (arguments '(#:tests? #f))  ; tests all use 'docker-compose'.
     (propagated-inputs
-     `(("python-click" ,python-click)
-       ("python-distro" ,python-distro)))
+     `(("python-distro" ,python-distro)))
     (native-inputs
-     `(
-       ("python-pytest" ,python-pytest)
-       ))
+     `(("python-pytest" ,python-pytest)))
     (home-page "https://github.com/ofek/userpath")
     (synopsis "Add locations to the user PATH")
     (description
