f7c04640095690980235bc570d6f8b6cec6b6cb9
Efraim Flashner <efraim@flashner.co.il>
2022-11-11 10:10:44 +0200

    dfsg-main: golang: Remove upstreamed package

--- dfsg/main/golang.scm
+++ dfsg/main/golang.scm
@@ -9228,41 +9228,6 @@ generate Go code.  Install it by building this program and making it accessible
 within your PATH with the name:")
     (license license:asl2.0)))
 
-(define-public go-google-golang-org-protobuf
-  (deprecated-package "go-github-com-golang-org-protobuf" go-github-com-golang-protobuf-proto))
-;  (package
-;    (name "go-google-golang-org-protobuf")
-;    (version "1.27.1")
-;    (source
-;      (origin
-;        (method git-fetch)
-;        (uri (git-reference
-;               (url "https://go.googlesource.com/protobuf")
-;               (commit (string-append "v" version))))
-;        (file-name (git-file-name name version))
-;        (sha256
-;          (base32 "0aszb7cv8fq1m8akgd4kjyg5q7g5z9fdqnry6057ygq9r8r2yif2"))))
-;    (build-system go-build-system)
-;    (arguments '(#:import-path "google.golang.org/protobuf"))
-;    (propagated-inputs
-;     (list go-github-com-google-go-cmp
-;        ;go-github-com-golang-protobuf)
-;        ))
-;    (home-page "https://google.golang.org/protobuf")
-;    (synopsis "Go support for Protocol Buffers")
-;    (description
-;      "This project hosts the Go implementation for
-;@url{https://developers.google.com/protocol-buffers,protocol buffers}, which is
-;a language-neutral, platform-neutral, extensible mechanism for serializing
-;structured data.  The protocol buffer language is a language for specifying the
-;schema for structured data.  This schema is compiled into language specific
-;bindings.  This project provides both a tool to generate Go code for the
-;protocol buffer language, and also the runtime implementation to handle
-;serialization of messages in Go.  See the
-;@url{https://developers.google.com/protocol-buffers/docs/overview,protocol
-;buffer developer guide} for more information about protocol buffers themselves.")
-;    (license license:bsd-3)))
-
 (define-public go-go-opencensus-io
   (package
     (name "go-go-opencensus-io")
