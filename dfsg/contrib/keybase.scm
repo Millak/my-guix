cf8912530d8fbb7d6a711a9cc12d4991f39ab80e
VÖRÖSKŐI András <voroskoi@gmail.com>
2023-10-23 11:56:07 +0200

    Add golang-check module imports

--- dfsg/contrib/keybase.scm
+++ dfsg/contrib/keybase.scm
@@ -23,6 +23,7 @@
   #:use-module (guix utils)
   #:use-module (guix build-system go)
   #:use-module (gnu packages golang)
+  #:use-module (gnu packages golang-check)
   #:use-module (gnu packages syncthing)
   #:use-module (dfsg main golang))
 
--- wip/gotosocial.scm
+++ wip/gotosocial.scm
@@ -24,6 +24,7 @@
   #:use-module (guix build-system go)
   #:use-module (gnu packages)
   #:use-module (gnu packages golang)
+  #:use-module (gnu packages golang-check)
   #:use-module (gnu packages sqlite)
   #:use-module (gnu packages syncthing)
   #:use-module (dfsg main golang))
