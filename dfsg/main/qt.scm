9d8cbe8512f7a0621b3716e00ae055dc9494701f
Efraim Flashner <efraim@flashner.co.il>
2026-03-08 15:55:28 +0200

    dfsg-main: qtwayland-helper: Don't use host's guile modules.

--- dfsg/main/qt.scm
+++ dfsg/main/qt.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2024 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2024, 2026 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is an addendum to GNU Guix.
 ;;;
@@ -18,6 +18,7 @@
 (define-module (dfsg main qt)
   #:use-module (guix packages)
   #:use-module (guix build-system trivial)
+  #:use-module (guix gexp)
   #:use-module (gnu packages qt)
   #:use-module (srfi srfi-1))
 
@@ -28,41 +29,41 @@
     (source #f)
     (build-system trivial-build-system)
     (arguments
-     '(#:modules ((guix build utils)
-                  (srfi srfi-26))
+     (list
        #:builder
-       (begin
-         (use-modules (guix build utils)
-                      (srfi srfi-26))
-         (let ((out (assoc-ref %outputs "out"))
-               (qtbase-5    (assoc-ref %build-inputs "qtbase-5"))
-               (qtbase      (assoc-ref %build-inputs "qtbase"))
-               (qtwayland-5 (assoc-ref %build-inputs "qtwayland-5"))
-               (qtwayland   (assoc-ref %build-inputs "qtwayland")))
-         (for-each mkdir-p
-                   (list (string-append out "/lib/qt5")
-                         (string-append out "/lib/qt6")
-                         (string-append out "/share")))
-         ;; We use copy-recursively instead of union-build because the
-         ;; union-build used for profile generation can't add other plugins
-         ;; to an existing union-build.
-         (for-each (cut copy-recursively <> (string-append out "/lib/qt5/plugins"))
-                   (list (string-append qtbase-5 "/lib/qt5/plugins")
-                         (string-append qtwayland-5 "/lib/qt5/plugins")))
-         (for-each (cut copy-recursively <> (string-append out "/lib/qt6/plugins"))
-                   (list (string-append qtbase "/lib/qt6/plugins")
-                         (string-append qtwayland "/lib/qt6/plugins")))
-         (for-each (cut copy-recursively <> (string-append out "/share/doc"))
-                   (list (string-append qtbase-5 "/share/doc")
-                         (string-append qtwayland-5 "/share/doc")
-                         (string-append qtbase "/share/doc")
-                         (string-append qtwayland "/share/doc")))
-         (copy-recursively (string-append qtwayland-5 "/lib/qt5/qml")
-                           (string-append out "/lib/qt5/qml"))
-         (copy-recursively (string-append qtwayland "/lib/qt6/qml")
-                           (string-append out "/lib/qt6/qml"))
-         (delete-file-recursively (string-append out "/share/doc/qt5"))
-         (delete-file-recursively (string-append out "/share/doc/qt6"))))))
+       (with-imported-modules '((guix build utils))
+         #~(begin
+             (use-modules (guix build utils)
+                          (srfi srfi-26))
+             (let ((out (assoc-ref %outputs "out"))
+                   (qtbase-5    (assoc-ref %build-inputs "qtbase-5"))
+                   (qtbase      (assoc-ref %build-inputs "qtbase"))
+                   (qtwayland-5 (assoc-ref %build-inputs "qtwayland-5"))
+                   (qtwayland   (assoc-ref %build-inputs "qtwayland")))
+             (for-each mkdir-p
+                       (list (string-append out "/lib/qt5")
+                             (string-append out "/lib/qt6")
+                             (string-append out "/share")))
+             ;; We use copy-recursively instead of union-build because the
+             ;; union-build used for profile generation can't add other plugins
+             ;; to an existing union-build.
+             (for-each (cut copy-recursively <> (string-append out "/lib/qt5/plugins"))
+                       (list (string-append qtbase-5 "/lib/qt5/plugins")
+                             (string-append qtwayland-5 "/lib/qt5/plugins")))
+             (for-each (cut copy-recursively <> (string-append out "/lib/qt6/plugins"))
+                       (list (string-append qtbase "/lib/qt6/plugins")
+                             (string-append qtwayland "/lib/qt6/plugins")))
+             (for-each (cut copy-recursively <> (string-append out "/share/doc"))
+                       (list (string-append qtbase-5 "/share/doc")
+                             (string-append qtwayland-5 "/share/doc")
+                             (string-append qtbase "/share/doc")
+                             (string-append qtwayland "/share/doc")))
+             (copy-recursively (string-append qtwayland-5 "/lib/qt5/qml")
+                               (string-append out "/lib/qt5/qml"))
+             (copy-recursively (string-append qtwayland "/lib/qt6/qml")
+                               (string-append out "/lib/qt6/qml"))
+             (delete-file-recursively (string-append out "/share/doc/qt5"))
+             (delete-file-recursively (string-append out "/share/doc/qt6")))))))
     (inputs
      `(("qtbase-5" ,qtbase-5)
        ("qtbase" ,qtbase)
