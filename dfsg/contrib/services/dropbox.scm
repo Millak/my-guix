50ed7d0296cb632f13f95e760b5d95d8fbc7770d
Efraim Flashner <efraim@flashner.co.il>
2026-03-16 11:21:01 +0200

    dfsg-contrib: Add dbxfs service

---
+++ dfsg/contrib/services/dropbox.scm
@@ -0,0 +1,97 @@
+;;; Copyright © 2025 Efraim Flashner <efraim@flashner.co.il>
+;;;
+;;; This file is an addendum to GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (dfsg contrib services dropbox)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services admin)
+  #:use-module (gnu home services shepherd)
+  #:use-module (guix records)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages file-systems)
+  #:use-module (ice-9 match)
+  #:export (dbxfs-configuration
+            home-dbxfs-service-type))
+
+(define %logdir
+  (string-append
+    (or (getenv "XDG_STATE_HOME")
+        (string-append (getenv "HOME") "/.local/state"))
+    "/log"))
+
+(define-record-type* <dbxfs-configuration>
+  dbxfs-configuration make-dbxfs-configuration
+  dbxfs-configuration?
+  (package      dbxfs-configuration-package
+                (default dbxfs))        ; package
+  (mountpoint   dbxfs-configuration-mountpoint
+                (default (string-append (getenv "HOME") "/Dropbox")))  ; string
+  (config-json  dbxfs-configuration-config-json
+                (default #f))           ; #f or string
+  (autostart?   dbxfs-configuration-autostart?
+                (default #t))
+  (verbosity    tailscaled-configuration-verbosity
+                (default 0)))           ; 0/1/2
+
+(define (dbxfs-user-shepherd-service config)
+  "Return a <dbxfs-service> for dbxfs with CONFIG."
+  (match-record config <dbxfs-configuration>
+                (package mountpoint config-json autostart? verbosity)
+    (list
+      (shepherd-service
+        (documentation "Provide access to Dropbox™")
+        (provision '(dropbox dbxfs))
+        ;(requirement '(networking))
+        (start #~(make-forkexec-constructor
+                   (list #$(file-append package "/bin/dbxfs")
+                         "--foreground"
+                         #$@(match verbosity
+                              (1 '("--verbose"))
+                              (2 '("--verbose" "--verbose"))
+                              (_ '()))
+                         #$@(if config-json
+                                #~("--config-file" #$config-json)
+                                '())
+                         #$mountpoint)
+                   #:log-file #$(string-append %logdir "/dbxfs.log")))
+        (stop #~(make-system-destructor
+                  #$(string-append "fusermount -u " mountpoint)))
+        #;
+        (actions
+         (list (shepherd-configuration-action
+                 #~(zero? (system* #$(file-append package "/bin/dbxfs")
+                                   #$@(if config-json
+                                          #~("--config-file" #$config-json)
+                                          '())
+                                   "--print-default-config-file")))))
+        (auto-start? autostart?)
+        (respawn? #f)))))
+
+(define %dbxfs-log-rotation
+  (list (string-append %logdir "/dbxfs.log")))
+
+(define home-dbxfs-service-type
+  (service-type
+    (name 'dbxfs)
+    (extensions
+      (list (service-extension home-shepherd-service-type
+                               dbxfs-user-shepherd-service)
+            (service-extension home-log-rotation-service-type
+                               (const %dbxfs-log-rotation))
+            (service-extension home-profile-service-type
+                               (compose list dbxfs-configuration-package))))
+    (default-value (dbxfs-configuration))
+    (description "Use dbxfs to keep files synced with Dropbox™.")))
