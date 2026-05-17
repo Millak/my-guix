b5911054af42aba69d3d44a28c56b8364dc6110d
Efraim Flashner <efraim@flashner.co.il>
2026-05-17 09:47:10 +0300

    dfsg-main: pgzero: Fix build and tests.

--- dfsg/main/pgzero.scm
+++ dfsg/main/pgzero.scm
@@ -1,4 +1,4 @@
-;;; Copyright © 2024 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2024, 2026 Efraim Flashner <efraim@flashner.co.il>
 ;;;
 ;;; This file is an addendum to GNU Guix.
 ;;;
@@ -16,14 +16,20 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (dfsg main pgzero)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix download)
+  #:use-module (guix git-download)
   #:use-module (guix build-system pyproject)
   #:use-module (guix build-system python)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages fontutils)
   #:use-module (gnu packages game-development)
-  #:use-module (gnu packages python-xyz))
+  #:use-module (gnu packages music)
+  #:use-module (gnu packages python-build)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages sdl))
 
 (define-public python-pgzero
   (package
@@ -31,12 +37,36 @@
     (version "1.2.1")
     (source
      (origin
-       (method url-fetch)
-       (uri (pypi-uri "pgzero" version))
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/lordmauve/pgzero")
+              (commit version)))
+       (file-name (git-file-name name version))
        (sha256
-        (base32 "01k1iv1qdy9kyizr3iysxqfmy10w38qvjfxx1hzarjr8y0hc1bcc"))))
+        (base32 "0wsbclznd1rqg0h3xpajqfmd8kwjdx99mbyxv1w23hr5rw5d0xrv"))))
     (build-system pyproject-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'check 'pre-check
+            (lambda _
+              (setenv "HOME" "/tmp")
+              (setenv "SDL_VIDEODRIVER" "dummy")
+              (setenv "SDL_AUDIODRIVER" "disk"))))))
     (propagated-inputs (list python-numpy python-pygame))
+    (native-inputs
+     (append
+       (list python-setuptools python-pytest)
+       ;; Additional programs for the tests
+       (if (%current-target-system)
+           '()
+           (list freetype
+                 portmidi
+                 sdl2
+                 sdl2-image
+                 sdl2-mixer
+                 sdl2-ttf))))
     (home-page "http://pypi.python.org/pypi/pgzero")
     (synopsis "Zero-boilerplate 2D games framework")
     (description "This package provides a zero-boilerplate 2D games framework
